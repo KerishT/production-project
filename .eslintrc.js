4c10542a678415f5123b547de880617b03251ac5
KerishT <kerish.turista@gmail.com>
2025-01-05 08:35:59 +0500

    add react testing library, add tests button, sidebar

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,10 +30,18 @@ module.exports = {
         'import/extensions': 0,
         'import/no-extraneous-dependencies': 1,
         'no-underscore-dangle': 0,
-        'i18next/no-literal-string': [2, { markupOnly: true }],
-        'max-len': [0, { ignoreComments: true }],
+        'i18next/no-literal-string': [2, { markupOnly: true, ignoreAttribute: ['data-testid'] }],
+        'max-len': [0, { ignoreComments: true, code: 100 }],
     },
     globals: {
         __IS_DEV__: true,
     },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 0,
+            },
+        },
+    ],
 };
--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+
 export default {
     clearMocks: true,
     testEnvironment: 'jsdom',
@@ -16,7 +18,13 @@ export default {
         'node',
     ],
     rootDir: '../../',
+    modulePaths: ['<rootDir>/src'],
     testMatch: [
-        '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
+        '<rootDir>src/**/*(*.)@(spec |test).[tj]s?(x)',
     ],
+    setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+    moduleNameMapper: {
+        '\\.s?css$': 'identity-obj-proxy',
+        '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+    },
 };
