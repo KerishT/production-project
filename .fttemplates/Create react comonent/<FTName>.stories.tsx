f9955352bdcfd5949b153ae1269b19d5e645542c
KerishT <kerish.turista@gmail.com>
2025-04-21 12:07:33 +0300

    add react component folder template

---
+++ .fttemplates/Create react comonent/<FTName>.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { <FTName> } from './<FTName>';
+
+export default {
+    title: '<slice>/<FTName>',
+    component: <FTName>,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof <FTName>>;
+
+const Template: ComponentStory<typeof <FTName>> = (args) => <<FTName> {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
