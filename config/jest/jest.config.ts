7382da2bd512da71256f6c747f6d2d4a506b9d1d
KerishT <kerish.turista@gmail.com>
2025-06-04 13:37:25 +0300

    add html reporter for unit tests

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -31,5 +31,14 @@ export default {
     moduleNameMapper: {
         '\\.s?css$': 'identity-obj-proxy',
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx')
-    }
+    },
+    reporters: [
+        'default',
+        ['jest-html-reporters', {
+            publicPath: '<rootDir>/reports/unit',
+            filename: 'report.html',
+            openReport: true,
+            inlineSource: true
+        }]
+    ]
 };
