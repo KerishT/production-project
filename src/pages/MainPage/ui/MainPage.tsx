9788aeb65c5f934c6909072cdb3d4459d4e2a518
KerishT <kerish.turista@gmail.com>
2025-06-25 08:07:11 +0300

    clear MainPage

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,16 +1,11 @@
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page';
-import { Counter } from '@/entities/Counter';
 
 const MainPage = () => {
     const { t } = useTranslation();
 
     return (
-        <Page>
-            {t('Главная')}
-
-            <Counter />
-        </Page>
+        <Page>{t('Главная')}</Page>
     );
 };
 
